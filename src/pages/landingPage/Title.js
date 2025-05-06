3a8ca07cbcd02a9de1218931ff7447498221d70c
esmaeilsadeghijob <esmaeilsadeghijob@gmail.com>
2025-05-06 14:04:47 -0700

    Refactoring

--- src/pages/landingPage/Title.js
+++ src/pages/landingPage/Title.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Layout, Menu, Divider, Image } from "antd";
+import {Layout, Menu, Divider, Image} from "antd";
 import "./Title.css";
 
-const { Content } = Layout;
+const {Content} = Layout;
 
 const Title = () => {
     return (
@@ -15,17 +15,20 @@ const Title = () => {
                         alt="Sample"
                         width="80%" /* بزرگ‌ترین حالت ممکن */
                         preview={false}
-                        style={{ borderRadius: "10px", boxShadow: "0px 4px 8px rgba(0,0,0,0.2)" }}
+                        style={{borderRadius: "10px", boxShadow: "0px 4px 8px rgba(0,0,0,0.2)"}}
                     />
                 </div>
 
                 {/* خط عمودی */}
-                <Divider type="vertical" className="divider" />
+                <Divider type="vertical" className="divider"/>
 
                 {/* بخش گزینه‌ها */}
                 <div className="options-section">
                     <h2>خدمات جاوتار</h2>
                     <Menu mode="vertical" theme="light" className="menu">
+                        <Menu.Item key="3">
+                            <a href="https://www.esmaeilsadeghi.ir/question">استعدادیابی</a>
+                        </Menu.Item>
                         <Menu.Item key="1">
                             <a href="https://www.esmaeilsadeghi.ir/signup">ثبت نام</a>
                         </Menu.Item>
@@ -34,9 +37,6 @@ const Title = () => {
                         </Menu.Item>
                         <Menu.Item key="4">
                             <a href="https://www.esmaeilsadeghi.ir/#projects">آموزش</a>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <a href="/jobs">کاریابی</a>
                         </Menu.Item>
                     </Menu>
                 </div>
